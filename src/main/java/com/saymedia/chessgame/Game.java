98349a5c24d2361ba9aaeee2d02ac3612577996e
Gil Harel <gil@usr2.bezeqint.net>
2015-06-13 15:49:12 +0300

    findViewById(R.id.fragment)

--- src/main/java/com/saymedia/chessgame/Game.java
+++ src/main/java/com/saymedia/chessgame/Game.java
@@ -1,50 +1,96 @@
 package com.saymedia.chessgame;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 
 public class Game extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        creatPieces();
+
+    }
+
+    public void creatPieces(){
+
+//        System.out.println("creatPieces v: " + v);
+
+//        System.out.println("findViewById: " + findViewById(R.id.fragment));
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.fragment);
+//        do{rl = (RelativeLayout)findViewById(R.id.layout1); }
+//        while(rl == null);
+        System.out.println("rl: " + rl);
+        ImageView wking = new ImageView(getApplicationContext());
+        wking.setImageResource(R.drawable.wking);
+        wking.setLayoutParams(place("A", 1));
+        System.out.println("wking: " + wking);
+        wking.setVisibility(View.VISIBLE);
+        rl.addView(wking);
+
+
     }
 
     public void backButton(View v){
         startActivity(new Intent("first.activity"));
     }
 
+    private int dp(double dp) {
 
-    private int dp(int dp)
-    {
         float density = getApplicationContext().getResources().getDisplayMetrics().density;
         return (int)(dp * density);
     }
 
-    public RelativeLayout.LayoutParams Place(){
-        RelativeLayout.LayoutParams A2 = new RelativeLayout.LayoutParams(100,100);
-        A2.setMargins(2, 0, dp(2000), 0);
+    public RelativeLayout.LayoutParams place(String x , int y){
 
-        RelativeLayout.LayoutParams A1 = new RelativeLayout.LayoutParams(0,0);
-        A1.setMargins(24, 415, 0, 132);
+        double X = 24;
+        double Y = 25;
+        double d = 36.9;
 
-        return A2;
+        RelativeLayout.LayoutParams L = new RelativeLayout.LayoutParams(dp(38),dp(38));
+        L.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        L.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+
+        switch (x){
+            case "A": L.leftMargin = dp(X) + 0*dp(d); break;
+            case "B": L.leftMargin = dp(X) + 1*dp(d); break;
+            case "C": L.leftMargin = dp(X) + 2*dp(d); break;
+            case "D": L.leftMargin = dp(X) + 3*dp(d); break;
+            case "E": L.leftMargin = dp(X) + 4*dp(d); break;
+            case "F": L.leftMargin = dp(X) + 5*dp(d); break;
+            case "G": L.leftMargin = dp(X) + 6*dp(d); break;
+            case "H": L.leftMargin = dp(X) + 7*dp(d); break;
+
+        }
+
+        switch (y){
+            case 1: L.bottomMargin = dp(Y) + 0*dp(d); break;
+            case 2: L.bottomMargin = dp(Y) + 1*dp(d); break;
+            case 3: L.bottomMargin = dp(Y) + 2*dp(d); break;
+            case 4: L.bottomMargin = dp(Y) + 3*dp(d); break;
+            case 5: L.bottomMargin = dp(Y) + 4*dp(d); break;
+            case 6: L.bottomMargin = dp(Y) + 5*dp(d); break;
+            case 7: L.bottomMargin = dp(Y) + 6*dp(d); break;
+            case 8: L.bottomMargin = dp(Y) + 7*dp(d); break;
+
+        }
+
+        return L;
     }
 
-    public void PiecesOnClick(View v){
-        ImageView wking = (ImageView) findViewById(R.id.wking);
-        RelativeLayout.LayoutParams A2 = new RelativeLayout.LayoutParams(135,135);
-        A2.setMargins(700, 500, 0, 0);
-
-        wking.setLayoutParams(A2);
+    public void piecesOnClick(View v){
+        v.setLayoutParams(place("H" , 3));
     }
 }
--- src/main/java/com/saymedia/chessgame/Welcome.java
+++ src/main/java/com/saymedia/chessgame/Welcome.java
@@ -3,9 +3,11 @@ package com.saymedia.chessgame;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Welcome extends ActionBarActivity {
@@ -14,6 +16,8 @@ public class Welcome extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        System.out.println("onCreate completed");
     }
 
     public void buttonOnClick(View v){
